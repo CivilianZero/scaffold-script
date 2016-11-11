71fa8e72a669b1dccf9b2b66eb2140dfc958d048
CivilianZero <Civilian.Protocol@gmail.com>
2016-11-11 16:19:09 -0500

    added more git automation

--- scaffold.sh
+++ scaffold.sh
@@ -5,6 +5,9 @@ echo "Input link to new git repository"
 read newgit
 git remote set-url origin $newgit
 git remote -v
+git add .
+git commit -m "initial commit"
+git push -u origin master
 echo "Would you like to run npm install? y/n"
 read yn
 [ "$yn" == "y" ] && npm i || echo "did not run npm i"
